0395e3b6d1af1c00b0cba446a5ca7649945e1045
janDeVisscherUHasselt <jan.devisscher@student.uhasselt.be>
2022-10-30 08:43:45 +0100

    setup database

---
+++ bankProject/database/migrations/2022_10_30_073311_create_account_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account', function (Blueprint $table) {
+            $table->increments('id'); // sleutel, autoincrement
+            $table->string('naam'); // Voegt een VARCHAR toe
+            $table->float('saldo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account');
+    }
+};
